1d9fe8604546001d4a924d09603d5999a55af196
Tenlucena <samuellucenasantos@gmail.com>
2025-05-20 21:08:01 -0300

    Create Questao_03.js

---
+++ Questao_03.js
@@ -0,0 +1,29 @@
+let nome = "João da Silva";
+let primeiraNota = "7";
+let segundaNota = "8.5";
+let multPrimeiraNota = primeiraNota*0.6;
+let multSegundaNota = segundaNota*0.4;
+let soma = multPrimeiraNota + multSegundaNota;
+
+alert("Nome do aluno: " + nome + "\nNota 1: " + primeiraNota + "\nNota 2: " + segundaNota);
+alert("Nome do aluno: " + nome + "\nNota final: " + soma);
+
+nome = "Samuel Lucena Santos";
+primeiraNota = 10;
+segundaNota = 10;
+multPrimeiraNota = primeiraNota*0.6;
+multSegundaNota = segundaNota*0.4;
+soma = multPrimeiraNota + multSegundaNota;
+
+alert("Nome do aluno: " + nome + "\nNota 1: " + primeiraNota + "\nNota 2: " + segundaNota);
+alert("Nome do aluno: " + nome + "\nNota final: " + soma);
+
+nome = "Mudesto";
+primeiraNota = 5;
+segundaNota = 7;
+multPrimeiraNota = primeiraNota*0.6;
+multSegundaNota = segundaNota*0.4;
+soma = multPrimeiraNota + multSegundaNota;
+
+alert("Nome do aluno: " + nome + "\nNota 1: " + primeiraNota + "\nNota 2: " + segundaNota);
+alert("Nome do aluno: " + nome + "\nNota final: " + soma);
